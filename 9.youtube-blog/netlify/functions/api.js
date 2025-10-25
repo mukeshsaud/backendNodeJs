f9a4ed2914e302909981325144e4c5215b83188e
mukesh saud <mukeshsaud@mukeshs-MacBook-Air.local>
2025-10-26 01:08:46 +0545

    6th  blog

--- 9.youtube-blog/netlify/functions/api.js
+++ 9.youtube-blog/netlify/functions/api.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>console.log("mongodb connected"
 
 app.set('view engine','ejs');
 // app.set("views",path.resolve("./views"))
-app.set('views', path.join(__dirname, '../../../views'));
+app.set('views', path.join(__dirname, '../../views'));
 
 
 
@@ -27,7 +27,7 @@ app.use(express.urlencoded({extended:false}))
 app.use(cookieParser());
 app.use(checkForAuthentication);
 // const publicpath=path.resolve("./public")
-const publicpath = path.join(__dirname, '../../../public');
+const publicpath = path.join(__dirname, '../../public');
 app.use(express.static(publicpath));
 
 
